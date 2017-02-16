74dc03d7a1553e2dbe10cbb8a5ad2a56232b8a65
Oliver <Oliver>
2017-02-16 17:58:43 +0000

    Updated DLL so a new flat object isn't created every time a query is made

--- FLATDLL2/FLATDLL2.cpp
+++ FLATDLL2/FLATDLL2.cpp
@@ -12,65 +12,103 @@
 #define GetCurrentDir _getcwd
 
 char cCurrentPath[FILENAME_MAX];
+FlatManager *m_flatMan;
+
+FlatManager::FlatManager()
+{
+	if (GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+	{
+		char* trimmedPath = trimwhitespace(cCurrentPath);
+		int len = strlen(trimmedPath);
+
+		m_flat = new FLATTest(trimmedPath, len);
+	}
+}
+
+FlatManager::~FlatManager()
+{
+	delete(m_flat);
+	delete(m_trimmedPath);
+}
+
+void FlatManager::PerformQuery(float p0, float p1, float p2, float p3, float p4, float p5)
+{
+	if(m_flat)
+		m_flat->performTest(p0, p1, p2, p3, p4, p5);
+}
+
+int  FlatManager::GetResCoordsNum()
+{
+	if (m_flat)
+		return m_flat->numCoords;
+	else
+		return -1;
+}
+
+std::vector<FLAT::spaceUnit>  FlatManager::GetResCoords()
+{
+	if(m_flat)
+		return m_flat->results;
+	else
+	{
+		return *(new std::vector<FLAT::spaceUnit>());
+	}
+}
+
+char* FlatManager::trimwhitespace(char *str)
+{
+	char *end;
+
+	// Trim leading space
+	while (isspace((unsigned char)*str)) str++;
+
+	if (*str == 0)  // All spaces?
+		return str;
+
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end)) end--;
+
+	// Write new null terminator
+	*(end + 1) = 0;
+
+	return str;
+}
 
 extern "C" {
 
-
-	extern DECLDIR char *trimwhitespace(char *str)
+	extern DECLDIR bool InitFlatManager()
 	{
-		char *end;
+		m_flatMan = new FlatManager();
+		if (m_flatMan == nullptr)
+			return false;
 
-		// Trim leading space
-		while (isspace((unsigned char)*str)) str++;
-
-		if (*str == 0)  // All spaces?
-			return str;
-
-		// Trim trailing space
-		end = str + strlen(str) - 1;
-		while (end > str && isspace((unsigned char)*end)) end--;
-
-		// Write new null terminator
-		*(end + 1) = 0;
-
-		return str;
-	}
-
-	extern DECLDIR int add(int a, int b)
-	{
-		return a+b;
-	}
-
-	extern DECLDIR int addtest(int a, int b)
-	{
-		return addt(a, b);
+		return true;
 	}
 
 
 	extern DECLDIR bool PerformQuery(float** resultVerts, int* resultLen, float p0, float p1, float p2, float p3, float p4, float p5)
 	{
-		if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
-		{
+		if (m_flatMan == nullptr)
 			return false;
+
+		m_flatMan->PerformQuery(p0, p1, p2, p3, p4, p5);
+
+		int resLen = m_flatMan->GetResCoordsNum();
+		*resultLen = resLen;
+		if (resLen != -1)
+		{
+			*resultVerts = new float[resLen];
+			std::vector<FLAT::spaceUnit> res = m_flatMan->GetResCoords();
+
+			for (int i = 0; i < resLen; i++) {
+				(*resultVerts)[i] = res[i];
+			}
+			return true;
 		}
 
-		char* trimmedPath = trimwhitespace(cCurrentPath);
-		int len = strlen(trimmedPath);
-		//char * path = new char[len+2];
-		//strcpy_s(path, len+2, trimmedPath);
+		return false;
 
-		FLATTest* Flat = new FLATTest(trimmedPath, len);
-		Flat->performTest(p0, p1, p2, p3, p4, p5);
-
-		*resultLen = Flat->numCoords;
-		*resultVerts = new float[Flat->numCoords];
-
-		for (int i = 0; i < Flat->numCoords; i++) {
-			(*resultVerts)[i] = Flat->results[i];
-		}
 		
-		//Destroy Flat?
-
-		return true;
 	}
 }
--- FLATDLL2/FLATDLL2.h
+++ FLATDLL2/FLATDLL2.h
@@ -12,15 +12,32 @@
 #include "FLAT\include\FLATTest.hpp"
 #include "FLAT\include\addtest.h"
 
+//Class to hold the initialised 'FlatTest' object to allow quicker querying
+class FlatManager
+{
+public:
+	FlatManager();
+	~FlatManager();
+	void PerformQuery(float p0, float p1, float p2, float p3, float p4, float p5);
+	int GetResCoordsNum();
+	std::vector<FLAT::spaceUnit> GetResCoords();
+
+private:
+	FLATTest* m_flat;
+	char* m_trimmedPath;
+	int m_pathLen;
+	char *trimwhitespace(char *str);
+};
+
+
+
 extern "C"
 {
+	extern DECLDIR bool InitFlatManager();
+
 	extern DECLDIR bool PerformQuery(float** resultVerts, int* resultLen, float p0, float p1, float p2, float p3, float p4, float p5);
-
-	extern DECLDIR int add(int a, int b);
-
-	extern DECLDIR int addtest(int a, int b);
-
-	extern DECLDIR char *trimwhitespace(char *str);
 }
+
+
 
 #endif
